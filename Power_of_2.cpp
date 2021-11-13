d2dc90c8598e67f145bafcbbb393c8b457891ab8
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2021-11-13 11:05:34 +0530

    Add files via upload

---
+++ Power_of_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+bool ispow2(int n){
+	if(n==0)
+		return false;
+	return ((n&(n-1))==0);
+}
+int main(){
+	int n;
+	cin>>n;
+	int ans=ispow2(n);
+	if(ans==1)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+	return 0;
+}
---
+++ check_if_kth_bitisset.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,k;
+	cin>>n>>k;
+	if(n&(1<<(k-1))!=0)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+	return 0;
+}
